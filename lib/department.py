679bb248530c9d7b9d955b888dd3d52175aa732e
MBURU <wawerumburu157@gmail.com>
2024-12-15 10:42:45 +0300

    changes

--- lib/department.py
+++ lib/department.py
@@ -1,6 +1,5 @@
 from __init__ import CURSOR, CONN
 
-
 class Department:
 
     def __init__(self, name, location, id=None):
@@ -10,3 +9,47 @@ class Department:
 
     def __repr__(self):
         return f"<Department {self.id}: {self.name}, {self.location}>"
+
+    @classmethod
+    def create_table(cls):
+        '''Creates the 'departments' table if it doesn't exist.'''
+        sql = """
+        CREATE TABLE IF NOT EXISTS departments (
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            location TEXT
+        )
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
+
+    @classmethod
+    def drop_table(cls):
+        '''Drops the 'departments' table if it exists.'''
+        CURSOR.execute("DROP TABLE IF EXISTS departments")
+        CONN.commit()
+
+    def save(self):
+        '''Saves a department to the database.'''
+        if self.id is None:
+            CURSOR.execute("INSERT INTO departments (name, location) VALUES (?, ?)", (self.name, self.location))
+            self.id = CURSOR.lastrowid
+            CONN.commit()
+
+    @classmethod
+    def create(cls, name, location):
+        '''Creates a new department instance and saves it to the database.'''
+        department = cls(name, location)
+        department.save()
+        return department
+
+    def update(self):
+        '''Updates the department record in the database.'''
+        CURSOR.execute("UPDATE departments SET name = ?, location = ? WHERE id = ?", 
+                       (self.name, self.location, self.id))
+        CONN.commit()
+
+    def delete(self):
+        '''Deletes the department record from the database.'''
+        CURSOR.execute("DELETE FROM departments WHERE id = ?", (self.id,))
+        CONN.commit()
